4e73cef2dbd11668be369013dfef876c7c18054a
1m1w1j1 <1mark1w1jones1@gmail.com>
2026-02-02 18:17:44 -0500

    Fix for left and right hand table.

--- startup.lua
+++ startup.lua
@@ -1,15 +1,15 @@
 term.clear()
 term.setCursorPos(1,1)
 
-max_fuel = turtle.getFuelLimit()
-current_fuel = turtle.getFuelLevel()
-fuel_percent = (current_fuel / max_fuel) * 100
-unit_id = os.getComputerID()
-label_id = os.getComputerLabel()
-left_hand = turtle.getEquippedLeft()
-right_hand = turtle.getEquippedRight()
-left_hand_name = left_hand[1]
-right_hand_name = right_hand[1]
+local max_fuel = turtle.getFuelLimit()
+local current_fuel = turtle.getFuelLevel()
+local fuel_percent = (current_fuel / max_fuel) * 100
+local unit_id = os.getComputerID()
+local label_id = os.getComputerLabel()
+local left_hand = turtle.getEquippedLeft()
+local right_hand = turtle.getEquippedRight()
+local left_hand_name, name = next(left_hand)
+local right_hand_name, name = next(right_hand)
 
 
 print("Turtle ID : "..unit_id)
