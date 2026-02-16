a497f7f82c5281e7faafee2b107653a9798a3fc9
Mark Jones <1mark1w1jones1@gmail.com>
2026-02-16 09:44:11 -0500

    added kelpfarm

---
+++ kelpFarm.lua
@@ -0,0 +1,56 @@
+term.clear()
+term.setCursorPos(1, 1)
+term.write("Kelp Farm")
+
+
+function gotoFarm()
+    turtle.up()
+    turtle.up()
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnLeft()
+end
+
+function harvestKelp()
+    if turtle.getFuelLevel() ~= "unlimited" and turtle.getFuelLevel() < 150 then
+        error("Not enough fuel (need ~150)")
+    end
+    turtle.digDown()
+    turtle.down()
+
+    for row = 1, 10 do
+        -- Process current row
+        turtle.digDown()
+        for col = 1, 9 do
+            turtle.dig()
+            turtle.forward()
+            turtle.digDown()
+        end
+
+        -- Move to next row if not finished
+        if row < 10 then
+            if row % 2 == 1 then
+                -- Odd row: Turn Right, Move, Turn Right
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.digDown()
+                turtle.turnRight()
+            else
+                -- Even row: Turn Left, Move, Turn Left
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.digDown()
+                turtle.turnLeft()
+            end
+        end
+    end
+
+    -- Return to starting height
+    turtle.up()
+end
+
+-- Uncomment to go to farm first
+gotoFarm()
+harvestKelp()
