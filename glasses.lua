d2ef836ac53417577d07bdb3bd25d1c690976819
Mark Jones <1mark1w1jones1@gmail.com>
2026-02-28 10:44:53 -0500

    feat: Implement a bouncing rectangle with a time display, replacing previous scanner visualization and removing scanner peripheral integration.

--- glasses.lua
+++ glasses.lua
@@ -1,18 +1,7 @@
 local canvas = peripheral.wrap("back").canvas()
 canvas.clear() -- Get rid of our previous clock
-local rect = canvas.addRectangle(0, 0, 100, 100, 0xFF0000FF)
 
 local group = canvas.addGroup({ 0, 0 })
-
--- Wrap the scanner peripheral
-local scanner = peripheral.find("plethora:module_scanner")
-
-rect.setSize(250, 30)
-rect.setAlpha(100)
-
-local text = canvas.addText({ x = 5, y = 5 }, "")
-text.setScale(3)
-
 
 -- Look, we add items to our group instead
 group.addRectangle(0, 0, 240, 30, 0xFF000064)
@@ -22,13 +11,25 @@ text.setScale(3)
 
 local x, y, dx, dy = 0, 0, 5, 5
 
+-- Compute the dimensions we can move within
+local width, height = canvas.getSize()
+width = width - 240
+height = height - 30
 
 while true do
-    local scanner_radius = 8
-    local scanner_width = scanner_radius * 2 + 1
+  -- Bounce the group around the canvas
+  x = x + dx
+  if x < 0 then x, dx = 0, -dx end
+  if x > width then x, dx = width, -dx end
 
-    local scanned = scanner.scan()
-    local function scanned_at(x, y, z)
-        return scanned[scanner_width ^ 2 * (x + scanner_radius) + scanner_width * (y + scanner_radius) + (z + scanner_radius) + 1]
-end
+  y = y + dy
+  if y < 0 then y, dy = 0, -dy end
+  if y > height then y, dy = height, -dy end
+
+  group.setPosition(x, y)
+
+  -- And update the time
+  text.setText("Time is " .. textutils.formatTime(os.time()))
+
+  sleep(0.05)
 end
