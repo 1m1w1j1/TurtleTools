08133a8e58232e7b444624d3bb24401502dec6e3
Mark Jones <1mark1w1jones1@gmail.com>
2026-02-28 10:31:24 -0500

    feat: Filter scanned blocks to only print specific ore ore types.

--- oreScan.lua
+++ oreScan.lua
@@ -1,5 +1,34 @@
 local scanner = peripheral.wrap("back")
 
 for _, block in pairs(scanner.scan()) do
-    print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    if block.name == "minecraft:coal_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:iron_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:gold_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:diamond_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:redstone_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:lapis_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:emerald_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:nether_quartz_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:nether_gold_ore" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
+    if block.name == "minecraft:nether_ancient_debris" then
+        print(("The block at %d, %d, %d is %s"):format(block.x, block.y, block.z, block.name))
+    end
 end
